24df7a8a047b64b05e1abe06c7b3e75eaa16e157
Vitaly _Vi Shukela <vi0oss@gmail.com>
2020-11-24 00:07:44 +0300

    More modes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 use std::net::SocketAddr;
 use std::net::{TcpListener,TcpStream};
 use anyhow::Result;
+use std::time::{Duration,Instant};
+use std::io::ErrorKind;
 
 /// Check specified TCP tunnel (forwarder) implementation for defects and missing features
 #[derive(argh::FromArgs)]
@@ -32,7 +34,7 @@ fn trivial_test_1(opts: &Opts) -> Result<()> {
     let s = g.join().unwrap()?;
     anyhow::ensure!(s.len() > 0, "Empty string received from the forwarder");
     anyhow::ensure!(s == "Hello", "String mismatch after passing through the forwarder"); 
-    println!("Trivial test 1 passed");
+    println!("[ OK ] Trivial test 1");
     Ok(())
 }
 
@@ -57,15 +59,134 @@ fn trivial_test_2(opts: &Opts) -> Result<()> {
 
     anyhow::ensure!(s.len() > 0, "Empty string received from the forwarder");
     anyhow::ensure!(s == "Hello", "String mismatch after passing through the forwarder"); 
-    println!("Trivial test 2 passed");
+    println!("[ OK ] Trivial test 2");
     Ok(())
 }
 
+/// Write as much data as possible to this nonblocking writer
+fn clog(mut s: impl std::io::Write) -> Result<usize> {
+    let buf = [0u8; 1024];
+    let mut writelen = 1024;
+    let mut waitctr = 6;
+    let mut written : usize = 0;
+    loop {
+        match s.write(&buf[0..writelen]) {
+            Ok(0) => anyhow::bail!("Write to socket returned 0?"),
+            Ok(x) => written += x,
+            Err(e) if e.kind() == ErrorKind::WouldBlock => {
+                if waitctr > 0 {
+                    sleep(25);
+                    waitctr-=1;
+                    continue;
+                } else if writelen > 1 {
+                    writelen = 1;
+                    continue;
+                }
+                return Ok(written);
+            }
+            Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => Err(e)?,
+        }
+    }
+}
+
+fn sleep(ms: u64) {
+    std::thread::sleep(Duration::from_millis(ms));
+}
+
+// Check if docket gets disconnected 
+fn check_closedness(mut s: &TcpStream, how_long_to_wait: Duration) -> Result<bool> {
+    let deadline = Instant::now() + how_long_to_wait;
+    loop {
+        if Instant::now() > deadline {
+            return Ok(false)
+        }
+
+        use std::io::Write;
+        //let buf=[0u8; 1];
+        match s.take_error() {
+            Ok(None) => (),
+            Ok(Some(e)) if e.kind() == ErrorKind::ConnectionAborted => return Ok(true),
+            Ok(Some(e)) if e.kind() == ErrorKind::ConnectionReset => return Ok(true),
+            Ok(Some(e)) if e.kind() == ErrorKind::BrokenPipe => return Ok(true),
+            Ok(Some(e)) => Err(e)?,
+            Err(e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) if e.kind() == ErrorKind::WouldBlock => (),
+            Err(e) => Err(e)?,
+        }
+
+        sleep(50);
+    }
+}
+
+/// Clog both directions of the tunnel by writing, but not reading the data.
+/// Then close of of the sockets. Will RST propagate to the other end?  
+fn closedetect(opts: &Opts, report: bool, clog1: bool, clog2: bool, whichcheck: bool, shutdown1: bool, shutdown2: bool, name: &'static str) -> Result<()> {
+    use std::io::{Read,Write};
+    let ss = TcpListener::bind(opts.listen)?;
+    let g  = std::thread::spawn(move || -> Result<TcpStream> {
+        let mut cc = ss.accept()?.0;
+        drop(ss);
+        cc.set_nonblocking(true)?;
+        if shutdown1 {
+            cc.shutdown(std::net::Shutdown::Write)?;
+        }
+        if clog1 {
+            let sz = clog(&mut cc)?;
+            if report {
+                println!("One direction buffer: {}", sz);
+            }
+        }
+        //sleep(5000);
+        Ok(cc)
+    });
+    let mut cs = TcpStream::connect(opts.connect)?;
+    cs.set_nonblocking(true)?;
+    if shutdown2 {
+        cs.shutdown(std::net::Shutdown::Write)?;
+    }
+    
+    if clog2 {
+        let sz = clog(&mut cs)?;
+        if report {
+            println!("The other direction buffer: {}", sz);
+        }
+    }
+
+    let cc = g.join().unwrap()?;
+
+    // Now both `cs` and `cc` sockets are fully clogged. Let's close one of them and see what happens to the other one.
+
+    sleep(100);
+
+    let s = if whichcheck {
+        drop(cs);
+        cc
+    } else {
+        drop(cc);
+        cs
+    };
+
+
+    if check_closedness(&s, Duration::from_millis(500))? {
+        println!("[ OK ] Clogged close test {} passed", name);
+    } else {
+        println!("[FAIL] Clogged close test {} failed!", name);
+    }
+
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let opts : Opts = argh::from_env(); 
 
     trivial_test_1(&opts)?;
     trivial_test_2(&opts)?;
+    closedetect(&opts, true, true, true, true, false,false,"1")?;
+    closedetect(&opts, false, true, true, false, false,false,"2")?;
+    closedetect(&opts, false, false, true, false, false,false,"3")?;
+    closedetect(&opts, false, true, false, true, false,false,"4")?;
+    closedetect(&opts, false, false, true, false, true,false,"5")?;
+    closedetect(&opts, false, true, false, true, false,true,"6")?;
     Ok(())
 }
