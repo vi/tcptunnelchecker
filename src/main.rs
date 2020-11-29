012ce1787f3f251bd16c3bb755edde83523e306d
Vitaly _Vi Shukela <vi0oss@gmail.com>
2020-11-29 03:52:32 +0300

    Refactor 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(unused)]
+
 
 use std::net::SocketAddr;
 use std::net::{TcpListener,TcpStream};
@@ -90,21 +90,21 @@ fn clog(mut s: impl std::io::Write) -> Result<usize> {
     }
 }
 
-type ClosingNotif = Option<std::sync::Arc<std::sync::atomic::AtomicBool>>;
+type ShFlag = Option<std::sync::Arc<std::sync::atomic::AtomicBool>>;
 
 /// Read and ignore all the data in a separate thread
-fn drain(mut s: impl std::io::Read + Send + 'static, close_notification: ClosingNotif) {
+fn drain(mut s: impl std::io::Read + Send + 'static, close_notification: ShFlag) {
     std::thread::spawn(move|| {
         let mut buf = [0u8; 1024];
         loop {
             match s.read(&mut buf  [0..]) {
                 Ok(0) => break,
-                Ok(x) => (),
+                Ok(_x) => (),
                 Err(e) if e.kind() == ErrorKind::WouldBlock => {
                     sleep(10);
                 }
                 Err(e) if e.kind() == ErrorKind::Interrupted => (),
-                Err(e) => break,
+                Err(_e) => break,
             }
         }
         if let Some(ref x) = close_notification {
@@ -119,7 +119,7 @@ fn sleep(ms: u64) {
 }
 
 // Check if docket gets disconnected 
-fn check_closedness(mut s: &TcpStream, how_long_to_wait: Duration, additional_close_notification: ClosingNotif) -> Result<bool> {
+fn check_closedness(s: &TcpStream, how_long_to_wait: Duration, additional_close_notification: ShFlag) -> Result<bool> {
     let deadline = Instant::now() + how_long_to_wait;
     loop {
         if Instant::now() > deadline {
@@ -132,8 +132,6 @@ fn check_closedness(mut s: &TcpStream, how_long_to_wait: Duration, additional_cl
             }
         }
 
-        use std::io::Write;
-        //let buf=[0u8; 1];
         match s.take_error() {
             Ok(None) => (),
             Ok(Some(e)) if e.kind() == ErrorKind::ConnectionAborted => return Ok(true),
@@ -149,62 +147,155 @@ fn check_closedness(mut s: &TcpStream, how_long_to_wait: Duration, additional_cl
     }
 }
 
-#[derive(Clone,Copy)]
+
+#[derive(Clone,Copy, Debug)]
+enum CloseDetectMode {
+    CloseIncomingCheckOutgoing,
+    CloseOutgoingCheckIncoming,
+}
+
+#[derive(Clone,Copy,Debug)]
+enum WritingPolicy {
+    Ignore,
+    Shutdown,
+    Clog,
+}
+#[derive(Clone,Copy,Debug)]
+enum ReadingPolicy {
+    Ignore,
+    Drain,
+}
+
+#[derive(Clone,Copy,Debug)]
 struct CloseDetectOpts {
     report_buffer_sizes : bool,
-    clog_incoming: bool,
-    clog_outgoing: bool,
-    check_incoming_for_closedness: bool,
-    shutdown_incoming_for_writing: bool,
-    shutdown_outgoing_for_writing: bool,
-    drain_incoming: bool,
-    drain_outgoing: bool,
-    experiment_name: &'static str,
+    outgoing_write: WritingPolicy,
+    outgoing_read: ReadingPolicy,
+    incoming_write: WritingPolicy,
+    incoming_read: ReadingPolicy,
+    mode: CloseDetectMode,
+}
+
+impl std::fmt::Display for CloseDetectOpts {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "out")?;
+        match self.outgoing_write {
+            WritingPolicy::Ignore => (),
+            WritingPolicy::Shutdown => write!(f, "Shut")?,
+            WritingPolicy::Clog => write!(f, "Clog")?,
+        }
+        match self.outgoing_read {
+            ReadingPolicy::Ignore => (),
+            ReadingPolicy::Drain => write!(f, "Drain")?,
+        }
+        match self.mode {
+            CloseDetectMode::CloseIncomingCheckOutgoing => write!(f, "Check")?,
+            CloseDetectMode::CloseOutgoingCheckIncoming => write!(f, "Close")?,
+        }
+
+        write!(f, "_in")?;
+        match self.incoming_write {
+            WritingPolicy::Ignore => (),
+            WritingPolicy::Shutdown => write!(f, "Shut")?,
+            WritingPolicy::Clog => write!(f, "Clog")?,
+        }
+        match self.incoming_read {
+            ReadingPolicy::Ignore => (),
+            ReadingPolicy::Drain => write!(f, "Drain")?,
+        }
+        match self.mode {
+            CloseDetectMode::CloseIncomingCheckOutgoing => write!(f, "Close")?,
+            CloseDetectMode::CloseOutgoingCheckIncoming => write!(f, "Check")?,
+        }
+        Ok(())
+    }
 }
 
 /// Clog both directions of the tunnel by writing, but not reading the data.
 /// Then close of of the sockets. Will RST propagate to the other end?  
 fn closedetect(opts: &Opts, cdo: CloseDetectOpts) -> Result<()> {
-    use std::io::{Read,Write};
+    match (cdo.incoming_read, cdo.outgoing_write) {
+        (ReadingPolicy::Drain, WritingPolicy::Clog) => return Ok(()),
+        _ => (),
+    }
+    match (cdo.outgoing_read, cdo.incoming_write) {
+        (ReadingPolicy::Drain, WritingPolicy::Clog) => return Ok(()),
+        _ => (),
+    }
+    match cdo.mode {
+        CloseDetectMode::CloseIncomingCheckOutgoing => {
+            match cdo.outgoing_write {
+                WritingPolicy::Clog => (),
+                _ => return Ok(()),
+            }
+        }
+        CloseDetectMode::CloseOutgoingCheckIncoming => {
+            match cdo.incoming_write {
+                WritingPolicy::Clog => (),
+                _ => return Ok(()),
+            }
+        }
+    }
+    match (cdo.incoming_write, cdo.outgoing_write) {
+        (WritingPolicy::Clog, _) => (),
+        (_, WritingPolicy::Clog) => (),
+        _ => return Ok(()),
+    }
+
     let ss = TcpListener::bind(opts.listen)?;
-    let g  = std::thread::spawn(move || -> Result<(TcpStream, ClosingNotif)> {
-        let mut cn : ClosingNotif = None;
+    let g  = std::thread::spawn(move || -> Result<(TcpStream, ShFlag)> {
+        let mut cn : ShFlag = None;
         let mut cc = ss.accept()?.0;
         drop(ss);
         cc.set_nonblocking(true)?;
-        if cdo.shutdown_incoming_for_writing {
-            cc.shutdown(std::net::Shutdown::Write)?;
-        }
-        if cdo.drain_incoming {
-            cn = Some(std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true)));
-            drain(cc.try_clone()?, cn.clone());
-        }
-        if cdo.clog_incoming {
-            let sz = clog(&mut cc)?;
-            if cdo.report_buffer_sizes {
-                println!("One direction buffer: {}", sz);
+        match cdo.incoming_read {
+            ReadingPolicy::Ignore => (),
+            ReadingPolicy::Drain => {
+                cn = Some(std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true)));
+                drain(cc.try_clone()?, cn.clone());
             }
         }
+        match cdo.incoming_write {
+            WritingPolicy::Ignore => (),
+            WritingPolicy::Shutdown => {
+                cc.shutdown(std::net::Shutdown::Write)?
+            }
+            WritingPolicy::Clog => {
+                let sz = clog(&mut cc)?;
+                if cdo.report_buffer_sizes {
+                    println!("One direction buffer: {}", sz);
+                }
+            }
+        }
+
         //sleep(5000);
         Ok((cc, cn))
     });
     let mut cs = TcpStream::connect(opts.connect)?;
-    let mut cs_close : ClosingNotif = None;
+    let mut cs_close : ShFlag = None;
     cs.set_nonblocking(true)?;
-    if cdo.shutdown_outgoing_for_writing {
-        cs.shutdown(std::net::Shutdown::Write)?;
-    }
-    
-    if cdo.drain_outgoing {
-        cs_close = Some(std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true)));
-        drain(cs.try_clone()?, cs_close.clone());
-    }
-    if cdo.clog_outgoing {
-        let sz = clog(&mut cs)?;
-        if cdo.report_buffer_sizes {
-            println!("The other direction buffer: {}", sz);
+
+
+    match cdo.outgoing_read {
+        ReadingPolicy::Ignore => (),
+        ReadingPolicy::Drain => {
+            cs_close = Some(std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true)));
+            drain(cs.try_clone()?, cs_close.clone());
         }
     }
+    match cdo.outgoing_write {
+        WritingPolicy::Ignore => (),
+        WritingPolicy::Shutdown => {
+            cs.shutdown(std::net::Shutdown::Write)?;
+        }
+        WritingPolicy::Clog => {
+            let sz = clog(&mut cs)?;
+            if cdo.report_buffer_sizes {
+                println!("The other direction buffer: {}", sz);
+            }
+        }
+    }
+
 
     let (cc,cc_close) = g.join().unwrap()?;
 
@@ -212,18 +303,22 @@ fn closedetect(opts: &Opts, cdo: CloseDetectOpts) -> Result<()> {
 
     sleep(100);
 
-    let (s, closenotif) = if cdo.check_incoming_for_closedness {
-        drop(cs);
-        (cc, cc_close)
-    } else {
-        drop(cc);
-        (cs, cs_close)
+    let (s, closenotif) = match cdo.mode {
+        CloseDetectMode::CloseIncomingCheckOutgoing => {
+            drop(cc);
+            (cs, cs_close)
+        }
+        CloseDetectMode::CloseOutgoingCheckIncoming => {
+            drop(cs);
+            (cc, cc_close)
+
+        }
     };
 
     if check_closedness(&s, Duration::from_millis(500), closenotif)? {
-        println!("[ OK ] Clogged close test {} passed", cdo.experiment_name);
+        println!("[ OK ] Clogged close test passed: {}", cdo);
     } else {
-        println!("[FAIL] Clogged close test {} failed!", cdo.experiment_name);
+        println!("[FAIL] Clogged close test failed: {}", cdo);
     }
 
     Ok(())
@@ -236,139 +331,43 @@ fn main() -> Result<()> {
 
     trivial_test_2(&opts)?;
 
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : true,
-        clog_incoming : true,
-        clog_outgoing : true,
-        check_incoming_for_closedness: true,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: false,
-        experiment_name: "1",
+    let mut report_buffer_sizes = true;
+
+    let mut cd_battery1 = |mode:CloseDetectMode|  {
+        let mut cd_battery2 = |incoming_write: WritingPolicy| {
+            let mut cd_battery3 = |outgoing_write: WritingPolicy| {
+                let mut cd_battery4 = |incoming_read:ReadingPolicy| {
+                    let mut cd_battery5 = |outgoing_read: ReadingPolicy| {
+                        let cdo = CloseDetectOpts {
+                            report_buffer_sizes,
+                            incoming_write,
+                            outgoing_write,
+                            incoming_read,
+                            outgoing_read,
+                            mode,
+                        };
+                        if let Err(e) = closedetect(&opts, cdo) {
+                            eprintln!("{}", e);
+                        }
+                        report_buffer_sizes = false;
+                    };
+                    cd_battery5(ReadingPolicy::Ignore);
+                    cd_battery5(ReadingPolicy::Drain);
+                };
+                cd_battery4(ReadingPolicy::Ignore);
+                cd_battery4(ReadingPolicy::Drain);
+            };
+            cd_battery3(WritingPolicy::Ignore);
+            cd_battery3(WritingPolicy::Clog);
+            cd_battery3(WritingPolicy::Shutdown);
+        };
+        cd_battery2(WritingPolicy::Ignore);
+        cd_battery2(WritingPolicy::Clog);
+        cd_battery2(WritingPolicy::Shutdown);
     };
-    closedetect(&opts, cdo)?;
 
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : true,
-        clog_outgoing : true,
-        check_incoming_for_closedness: false,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: false,
-        experiment_name: "2",
-    };
-    closedetect(&opts, cdo)?;
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : false,
-        clog_outgoing : true,
-        check_incoming_for_closedness: false,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: false,
-        experiment_name: "3",
-    };
-    closedetect(&opts, cdo)?;
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : true,
-        clog_outgoing : false,
-        check_incoming_for_closedness: true,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: false,
-        experiment_name: "4",
-    };
-    closedetect(&opts, cdo)?;
-
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : false,
-        clog_outgoing : true,
-        check_incoming_for_closedness: false,
-        shutdown_incoming_for_writing: true,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: false,
-        experiment_name: "5",
-    };
-    closedetect(&opts, cdo)?;
-
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : true,
-        clog_outgoing : false,
-        check_incoming_for_closedness: true,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: true,
-        drain_incoming: false,
-        drain_outgoing: false,
-        experiment_name: "6",
-    };
-    closedetect(&opts, cdo)?;
-
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : false,
-        clog_outgoing : true,
-        check_incoming_for_closedness: false,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: true,
-        experiment_name: "7",
-    };
-    closedetect(&opts, cdo)?;
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : true,
-        clog_outgoing : false,
-        check_incoming_for_closedness: true,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: true,
-        drain_outgoing: false,
-        experiment_name: "8",
-    };
-    closedetect(&opts, cdo)?;
-
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : false,
-        clog_outgoing : true,
-        check_incoming_for_closedness: false,
-        shutdown_incoming_for_writing: true,
-        shutdown_outgoing_for_writing: false,
-        drain_incoming: false,
-        drain_outgoing: true,
-        experiment_name: "9",
-    };
-    closedetect(&opts, cdo)?;
-
-    let cdo = CloseDetectOpts {
-        report_buffer_sizes : false,
-        clog_incoming : true,
-        clog_outgoing : false,
-        check_incoming_for_closedness: true,
-        shutdown_incoming_for_writing: false,
-        shutdown_outgoing_for_writing: true,
-        drain_incoming: true,
-        drain_outgoing: false,
-        experiment_name: "10",
-    };
-    closedetect(&opts, cdo)?;
+    cd_battery1(CloseDetectMode::CloseIncomingCheckOutgoing);
+    cd_battery1(CloseDetectMode::CloseOutgoingCheckIncoming);
 
     Ok(())
 }
